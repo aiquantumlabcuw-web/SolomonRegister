681b23b423111ef29c6aa9d8252ced08056da967
Sanagala Rishi Preetham <rishipreetham18@gmail.com>
2025-10-02 16:37:41 -0500

    updated Global config

---
+++ makerspace/config/GlobalConfig.js
@@ -0,0 +1,8 @@
+const GlobalConfig = {
+    //nodeUrl: "https://www.cuwcs.com/mks"
+    //nodeUrl: "http://localhost:3000"
+     nodeUrl:"http://localhost:3000"  
+  };
+  
+  export default GlobalConfig;
+  
