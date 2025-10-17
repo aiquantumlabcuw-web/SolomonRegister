4a8b49b0095f8d4476d9e07ea64d592da990c806
Sanagala Rishi Preetham <rishipreetham18@gmail.com>
2025-10-16 19:35:08 -0500

    Updated the backend URL

--- makerspace/config/GlobalConfig.js
+++ makerspace/config/GlobalConfig.js
@@ -1,7 +1,7 @@
 const GlobalConfig = {
     //nodeUrl: "https://www.cuwcs.com/mks"
     //nodeUrl: "http://localhost:3000"
-     nodeUrl:"http://localhost:3000"  
+     nodeUrl:"https://solomonregister-production.up.railway.app"  
   };
   
   export default GlobalConfig;
